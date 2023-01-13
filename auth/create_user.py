57448fbda9aa90fc9a86ef8b475712a01ce1bde9
Michael Jarvis <mjfullstack@gmail.com>
2023-01-13 00:43:45 +0000

    update create_user.py

--- auth/create_user.py
+++ auth/create_user.py
@@ -5,9 +5,9 @@ from auth.funcs import run
 
 
 async def create_user(username, password):
-    user = User(username=username, password=password)
-    await user.save()
+    await User.create(username=username, password=password)
+    print(f"{username} user created")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run(create_user(input("username: "), getpass("password: ")))
