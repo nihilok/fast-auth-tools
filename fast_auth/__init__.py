cd956d9567e684f3ef25ba26a621f66d890261bd
Michael Jarvis <mjfullstack@gmail.com>
2023-01-15 16:09:25 +0000

    only import single func

--- fast_auth/__init__.py
+++ fast_auth/__init__.py
@@ -1,12 +1,10 @@
 from .user import User, get_current_user
-from .app_setup import set_cors_origins, add_auth_routes, fast_auth
+from .app_setup import fast_auth
 from .settings import settings
 
 __all__ = [
     "User",
     "get_current_user",
-    "add_auth_routes",
     "fast_auth",
-    "set_cors_origins",
     "settings",
 ]
